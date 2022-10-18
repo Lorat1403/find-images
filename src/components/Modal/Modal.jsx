6c92fad8816b9fc5ff69be111abe59c7e9f5034a
Lora <lorat1403@gmail.com>
2022-10-18 17:17:02 +0300

    correct app&modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -38,38 +38,3 @@ Modal.propTypes = {
 };
 
 export default Modal;
-
-// export default class Modal extends Component {
-//   componentDidMount() {
-//     window.addEventListener('keydown', this.handleKeyDown);
-//   }
-
-//   componentWillUnmount() {
-//     window.removeEventListener('keydown', this.handleKeyDown);
-//   }
-
-//   handleKeyDown = e => {
-//     if (e.code === 'Escape') {
-//       this.props.onClose();
-//     }
-//   };
-
-//   handleBackDropClick = e => {
-//     if (e.currentTarget === e.target) {
-//       this.props.onClose();
-//     }
-//   };
-
-//   render() {
-//     return createPortal(
-//       <div className="Overlay" onClick={this.handleBackDropClick}>
-//         <div className="Modal">{this.props.children}</div>
-//       </div>,
-//       modalRoot
-//     );
-//   }
-// }
-
-// Modal.propTypes = {
-//   onClose: PropTypes.func.isRequired,
-// };
